a337f159ff762cb8db0978ded445ebf2511b7713
Filip <momotfilip@gmail.com>
2022-11-07 16:59:42 +0100

    Basics

--- snake.py
+++ snake.py
@@ -1,2 +1,28 @@
-print("Poggers")
-print("Poggers")
+import pygame as pg
+import random
+
+
+class Snake:
+
+    def __init__(self,w=640,h=480):
+        self.w = w
+        self.h = h
+
+        # init display
+
+        # init game state
+
+    def play_step(self):
+        pass
+
+if __name__ == '__main__':
+    game = Snake()
+
+    #Loop of the game
+    while True:
+        game.play_step()
+
+        # if game over then break
+
+
+    pg.quit()
